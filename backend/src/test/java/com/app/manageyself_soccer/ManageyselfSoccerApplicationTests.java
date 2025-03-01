74dbfa1c1f2be72f1cad359703e55f1866236ac8
cruly1 <pavai.viktor2@gmail.com>
2025-03-01 11:26:40 +0100

    backend initialized

---
+++ backend/src/test/java/com/app/manageyself_soccer/ManageyselfSoccerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.manageyself_soccer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManageyselfSoccerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
