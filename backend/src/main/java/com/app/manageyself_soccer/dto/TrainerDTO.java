6a9b321d324500987b4cb544bab429c31719be51
cruly1 <pavai.viktor2@gmail.com>
2025-03-04 20:39:46 +0100

    asd2

--- backend/src/main/java/com/app/manageyself_soccer/dto/TrainerDTO.java
+++ backend/src/main/java/com/app/manageyself_soccer/dto/TrainerDTO.java
@@ -8,7 +8,6 @@ import lombok.*;
 @NoArgsConstructor
 @Builder
 public class TrainerDTO {
-    private Long id;
     private String name;
     private String briefContent;
     private String imageName;
