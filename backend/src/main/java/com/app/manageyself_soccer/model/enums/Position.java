921d06fdbbd04cf8bad77f7517b660f2495b9947
cruly1 <pavai.viktor2@gmail.com>
2025-05-19 18:15:30 +0200

    english positions XDDDDDD

--- backend/src/main/java/com/app/manageyself_soccer/model/enums/Position.java
+++ backend/src/main/java/com/app/manageyself_soccer/model/enums/Position.java
@@ -1,8 +1,8 @@
 package com.app.manageyself_soccer.model.enums;
 
 public enum Position {
-    CSATAR,
-    KAPUS,
-    VEDO,
-    KOZEPPALYAS
+    ATTACKER,
+    GOALKEEPER,
+    DEFENDER,
+    MIDFIELDER
 }
